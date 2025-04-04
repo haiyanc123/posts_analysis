e4b5b0f7098f13bd3bb3a20a3f560471cb560dd6
Emily Hai <emilyhai@Emilys-MacBook-Pro.local>
2025-04-04 15:48:14 -0500

    reorganize the structure

--- backend/app.py
+++ backend/app.py
@@ -2,14 +2,14 @@ from flask import Flask
 from flasgger import Swagger
 
 from api.user_api import users_bp
-from api.post_api import posts_bp
+# from api.post_api import posts_bp
 
 app = Flask(__name__)
 swagger = Swagger(app)
 
 # Register blueprints
 app.register_blueprint(users_bp, url_prefix='/user')
-app.register_blueprint(posts_bp, url_prefix='/post')
+# app.register_blueprint(posts_bp, url_prefix='/post')
 
 if __name__ == '__main__':
     app.run(debug=True)
