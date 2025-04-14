e7aed713999603788ef544fa829b797dcc19e018
davidg-12 <davidguo81@gmail.com>
2025-04-13 20:38:48 -0500

    created base for project page

---
+++ frontend/src/pages/ProjectPage/ProjectContainer/ProjectContainer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function ProjectContainer() {
+  return <div>Temp Project Container</div>;
+}
+
+ProjectContainer.displayName = "project-container";
+
+export default ProjectContainer;
--- frontend/src/pages/ProjectPage/ProjectPage.jsx
+++ frontend/src/pages/ProjectPage/ProjectPage.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import ProjectContainer from "./ProjectContainer/ProjectContainer";
 
 function ProjectPage() {
-  return <div>Temp Project Page</div>;
+  return (
+    <div>
+      <h2>Project</h2>
+      <hr></hr>
+      <ProjectContainer />
+    </div>
+  );
 }
 
 ProjectPage.displayName = "project-page";
