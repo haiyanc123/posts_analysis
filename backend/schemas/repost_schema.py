33f7d1f2c9a5d534d30ee60cc4077a3455cee850
Emily Hai <emilyhai@Emilys-MacBook-Pro.local>
2025-04-21 16:23:10 -0500

    fix bugs: coverage issue; empty space

--- backend/schemas/repost_schema.py
+++ backend/schemas/repost_schema.py
@@ -1,11 +1,14 @@
 from marshmallow import Schema,fields,validate
 
+from utils.common_util import not_blank
+
+
 class RePostCreateSchema(Schema):
-    repo_username= fields.String(required=True,validate=validate.Length(max=40))
-    repo_social_media=fields.String(required=True, validate=validate.Length(max=255))
+    repo_username= fields.String(required=True,validate=[not_blank,validate.Length(min=1,max=40)])
+    repo_social_media=fields.String(required=True, validate=[not_blank,validate.Length(min=1,max=255)])
     repo_time=fields.DateTime(format="%Y-%m-%d %H:%M:%S")
-    post_username= fields.String(required=True,validate=validate.Length(max=40))
-    post_social_media=fields.String(required=True, validate=validate.Length(max=255))
+    post_username= fields.String(required=True,validate=[not_blank,validate.Length(min=1,max=40)])
+    post_social_media=fields.String(required=True, validate=[not_blank,validate.Length(min=1,max=255)])
     post_time=fields.DateTime(format="%Y-%m-%d %H:%M:%S")
 
 
